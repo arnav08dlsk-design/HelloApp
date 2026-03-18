bdd1ded36aadef91ca8c69efc9b29e43a042fce1
Arnav Sahoo <arnav08dlsk@gmail.com>
2026-03-18 10:17:55 +0530

    Restore README, UC1, UC2 into UC3 feature branch

---
+++ UC1.java
@@ -0,0 +1,17 @@
+public class UC1 {
+<<<<<<< HEAD
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
+=======
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+>>>>>>> 274250175586e7fc1056a5c9a866df74f07bf58c
